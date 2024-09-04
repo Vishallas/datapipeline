f79c2c2cd8406438c9f1a439eaaac570488b40d2
Vishal S <vishal-pt7653@vishal-pt7653.csez.zohocorpin.com>
2024-09-04 15:26:59 +0530

    feat: batch producer

--- DataGenerator/src/main/java/org/example/Main.java
+++ DataGenerator/src/main/java/org/example/Main.java
@@ -1,35 +1,34 @@
 package org.example;
 
-
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Main {
     static final int THREAD_COUNT = 5;
-//    static final String TOPIC = "hadoop_data";
 
-    public static void main(String[] args) {
+    //static final String TOPIC = "hadoop_data";
+
+
+    public static void main(String[] args) throws  Exception{
         final Logger log = LoggerFactory.getLogger(Main.class);
         log.info("Logger initialized");
 
-
-
-        Thread[] threads = new Thread[THREAD_COUNT];
-        for (int i = 0; i < THREAD_COUNT; i++) {
-            threads[i] = new Thread(new ProducerUtil(i));
-            log.info("[ ] Thread " + i + " Created.");
-            threads[i].start();
-            log.info("[ ] Thread " + i + " Started.");
-        }
-
-        try {
-            for (Thread t : threads)
-                t.join();
-        } catch (InterruptedException e) {
-            log.error("Exception : ", e);
-        } finally {
-            log.info("Closing Producer");
-        }
+//        System.out.println(ipGenerator());
+//        Thread[] threads = new Thread[THREAD_COUNT];
+//        for (int i = 0; i < THREAD_COUNT; i++) {
+//            threads[i] = new Thread(new ProducerUtil(i));
+//            log.info("[ ] Thread " + i + " Created.");
+//            threads[i].start();
+//            log.info("[ ] Thread " + i + " Started.");
+//        }
+//
+//        try {
+//            for (Thread t : threads)
+//                t.join();
+//        } catch (InterruptedException e) {
+//            log.error("Exception : ", e);
+//        } finally {
+//            log.info("Closing Producer");
+//        }
     }
 }
--- DataGenerator/src/main/java/org/example/ProducerUtil.java
+++ DataGenerator/src/main/java/org/example/ProducerUtil.java
@@ -1,71 +1,201 @@
 package org.example;
+//import io.whitfin.siphash.SipHasher;
+//import org.apache.commons.codec.digest.MurmurHash3;
+
 import org.apache.kafka.clients.producer.*;
 import org.apache.kafka.common.serialization.StringSerializer;
-import org.apache.kafka.common.serialization.VoidSerializer;
+import org.json.JSONArray;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 
 import org.json.JSONObject;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 
 class RandomStringGenerator {
-    public static String getData(){
+    private final MessageDigest md;
+    private final DateTimeFormatter formatter;
+    private final String[] userAgentList;
+    private final String[] urlList;
+    private final Random random;
+    private Instant currentTime= null;
+    private final ZoneId zoneId;
 
-        // Data Formate and order
+    RandomStringGenerator() throws NoSuchAlgorithmException {
+        this.md = MessageDigest.getInstance("md5");
+        this.formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        this.userAgentList = assignUserAgent();
+        this.urlList = assignUrl();
+        this.random =new Random();
+        this.zoneId = ZoneId.of("UTC");
+    }
+    private String md5UUID(String ip, String userAgent){
 
-        //    uuid | visits | sid | eventtime | uvid | ip | user_agent | pageurl_trim
+        String input = ip+userAgent;
+        byte[] hashBytes = md.digest(input.getBytes());
 
+        // Create a UUID from the first 16 bytes of the MD5 hash
+        long mostSigBits = 0;
+        long leastSigBits = 0;
 
-        JSONObject data = new JSONObject();
+        for (int i = 0; i < 8; i++) {
+            mostSigBits |= ((long) (hashBytes[i] & 0xff)) << (8 * (7 - i));
+        }
+        for (int i = 8; i < 16; i++) {
+            leastSigBits |= ((long) (hashBytes[i] & 0xff)) << (8 * (15 - i));
+        }
 
-        data.put("uuid","000028e1-3e58-498a-8f4e-67a3fa7dbe89_d587"); // UUID
-        data.put("visits",1); // Visit Count
-        data.put("sid","-8.96494E+018");
-        data.put("eventtime","2024-08-31 00:42:48"); // Event Time
-        data.put("uvid","cb07f9bd-8dc1-4096-a657-6f19008f2174-2"); //UVID
-        data.put("ip","182.65.217.47"); //IP
-        data.put("user_agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36");
-        data.put("pageurl_trim","zoho.com/mail/"); //pageurl_trim
+        return (new UUID(mostSigBits, leastSigBits)).toString();
+    }
 
-        return data.toString();
+    private String[] assignUserAgent(){
+        return new String[] {
+                "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/37.0.2062.94 Chrome/37.0.2062.94 Safari/537.36"
+                ,"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36"
+                ,"Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko"
+                ,"Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.0"
+                ,"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/600.8.9 (KHTML, like Gecko) Version/8.0.8 Safari/600.8.9"
+                ,"Mozilla/5.0 (iPad; CPU OS 8_4_1 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Version/8.0 Mobile/12H321 Safari/600.1.4"
+                ,"Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36"
+                ,"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36"
+                ,"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.10240"
+                ,"Mozilla/5.0 (Windows NT 6.3; WOW64; rv:40.0) Gecko/20100101 Firefox/40.0"
+                ,"Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko"
+                ,"Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36"
+                ,"Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv:11.0) like Gecko"
+                ,"Mozilla/5.0 (Windows NT 10.0; WOW64; rv:40.0) Gecko/20100101 Firefox/40.0"
+                ,"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/600.7.12 (KHTML, like Gecko) Version/8.0.7 Safari/600.7.12"
+                ,"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36"
+                ,"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:40.0) Gecko/20100101 Firefox/40.0"
+                ,"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/600.8.9 (KHTML, like Gecko) Version/7.1.8 Safari/537.85.17"
+                ,"Mozilla/5.0 (iPad; CPU OS 8_4 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Version/8.0 Mobile/12H143 Safari/600.1.4"
+                ,"Mozilla/5.0 (iPad; CPU OS 8_3 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Version/8.0 Mobile/12F69 Safari/600.1.4"
+            };
+    }
 
+    private String[] assignUrl(){
+        return new String[] {
+                "zoho.com/phonebridge/developer/v3/call-control.html",
+                "zoho.com/in/inventory/inventory-dictionary/unit-of-measure.html",
+                "zoho.com/expense/#/trips/3260118000001599155",
+                "help.zoho.com/portal/en/kb/people/search/leave carry",
+                "help.zoho.com/portal/en/kb/people/zoho-people-4-0/employee-handbook-zoho-people-4-0/employee-leave/articles/zoho-people-employee-leave",
+                "help.zoho.com/portal/en/kb/people/administrator-guide/leave/overview/articles/leave-service-overview#What_is_Leave_Service_in_Zoho_People",
+                "help.zoho.com/portal/en/kb/people/administrator-guide/introduction/articles/what-is-zoho-people#Services_and_features_of_Zoho_People",
+                "zoho.com/in/books/help/transaction-approval/#approving",
+                "zoho.com/sheet/analyze.html",
+                "zoho.com/ipr-complaints.html",
+                "zoho.com/abuse-policy/",
+                "zoho.com/commerce/index1.html",
+                "store.zoho.in/html/store/index.html#subscription",
+                "zoho.com/id/forms/",
+                "help.zoho.com/portal/en/kb/vault/user-guide/sharing-passwords/articles/vault-add-passwords-notes-documents#Creating_custom_categories",
+                "zoho.com/ae/books/pricing/pricing-comparison.html",
+                "help.zoho.com/portal/en/kb/people/administrator-guide/attendance-management/settings/articles/breaks#Configuring_breaks",
+                "zoho.com/in/expense/#/expensereports/1471609000000640099",
+                "zoho.com/projects/project-milestones.html",
+                "help.zoho.com/portal/en/kb/workdrive/migrations/google/articles/migrate-from-g-suite-drive-to-zoho-workdrive#i_Create_your_own_Google_Cloud_app"
+        };
+
+    }
+    private String randUserAgent(){
+        int randomNumber= random.nextInt(userAgentList.length);
+        return userAgentList[randomNumber];
+    }
+
+    private String randUrl(){
+        int randomNumber = random.nextInt(urlList.length);
+        return urlList[randomNumber];
+    }
+
+    private String ipGenerator() {
+
+        Random random = new Random();
+
+        // Generate each of the 4 octets
+        int firstOctet = 180;
+        int secondOctet = 0;
+        int thirdOctet = random.nextInt(256); // 0 - 255
+        int fourthOctet = random.nextInt(256); // 0 - 255
+
+        return String.format("%d.%d.%d.%d", firstOctet, secondOctet, thirdOctet, fourthOctet);
+    }
+
+    private String getTime(){
+        if(currentTime == null){
+            currentTime = Instant.now();
+        }else {
+            long maxIntervalMillis = 15 * 60 * 1000;
+            // Minimum interval in milliseconds
+            long minIntervalMillis = 1000;
+            long intervalMillis = minIntervalMillis + (long) (random.nextDouble() * (maxIntervalMillis - minIntervalMillis));
+            currentTime = currentTime.plusMillis(intervalMillis);
+        }
+
+        return formatter.format(currentTime.atZone(zoneId));
+    }
+
+    public String getBatch(){
+        int BATCH_SIZE = 30;
+        JSONArray jsonArray = new JSONArray();
+
+        String ip = ipGenerator();
+        String userAgent = randUserAgent();
+
+        for(int i = 0;i<BATCH_SIZE;i++){
+            JSONObject jobj = new JSONObject();
+            jobj.put("ip",ip); //IP
+            jobj.put("user_agent", userAgent);
+            jobj.put("pageurl_trim","zoho.com/mail/");
+            jobj.put("eventtime",getTime());
+            jsonArray.put(jobj);
+        }
+
+        return jsonArray.toString();
     }
 }
 
 public class ProducerUtil implements Runnable {
     final String topicName = "hadoop_data";
-    final int key;
+    final int partition;
     ProducerUtil (int key){
-        this.key = key;
+        this.partition = key;
     }
 
     @Override
-    public void run() {
-        Properties prop = new Properties();
-        prop.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092");
-        prop.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        prop.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-
-
+    public void run(){
         Logger log = LoggerFactory.getLogger(ProducerUtil.class);
-        try(KafkaProducer<Void, String> producer = new KafkaProducer<>(prop)) {
-            while (true) {
-                Thread.sleep(2000);
-                String data = RandomStringGenerator.getData();
+        try {
+            RandomStringGenerator randGent = new RandomStringGenerator();
+            Properties prop = new Properties();
+            prop.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092");
+            prop.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+            prop.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+            try (KafkaProducer<Void, String> producer = new KafkaProducer<>(prop)) {
+                while (true) {
+                    Thread.sleep(2000);
+                    String data = randGent.getBatch();
 
-                ProducerRecord<Void, String> producerRecord = new ProducerRecord<>(topicName, key, null,data);
-                producer.send(producerRecord, (recordMetadata, e) ->
-                        log.info("Published to " + recordMetadata.topic()
-                                + ", Key " + key
-                                + ", Partition " + recordMetadata.partition()
-                                + ", timestamp " + recordMetadata.timestamp()
-                        )
-                );
+                    ProducerRecord<Void, String> producerRecord = new ProducerRecord<>(topicName, partition, null, data);
+                    producer.send(producerRecord, (recordMetadata, e) ->
+                            log.info("Published to " + recordMetadata.topic()
+                                    + ", Key " + null
+                                    + ", Partition " + recordMetadata.partition()
+                                    + ", timestamp " + recordMetadata.timestamp()
+                            )
+                    );
+                }
+            } catch (Exception e) {
+                log.error("Error occured ", e);
             }
-        } catch (Exception e) {
-            log.error("Error occured ", e);
-            throw new RuntimeException(e);
+        }catch (Exception e){
+            log.error("Error Generated", e);
         }
+
     }
 }
